b6923a31e78565f5190d42ffc7256ae276ae7da5
warlock <warlock39@gmail.com>
2020-04-05 02:44:47 +0300

    Gender and age stat of deaths (static)

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -73,6 +73,51 @@ class IndexController extends AbstractController
         }
         return $this->json($data);
     }
+    /**
+     * @Route("/api/deaths-gender-age", name="deathsGenderAge")
+     */
+    public function deathsGenderAge(): JsonResponse
+    {
+        $actual = [
+            'gender' => [
+                'male' => 9,
+                'female' => 19,
+            ],
+            'age' => [
+                '30-39' => 2,
+                '40-49' => 2,
+                '50-59' => 9,
+                '60-69' => 9,
+                '70-79' => 5,
+                '80-89' => 1,
+            ],
+        ];
+        $data = [
+            'actual' => $actual,
+            '2020-04-04' => $actual,
+            '2020-04-02' => [
+                'gender-age' => [
+                    'male' => [
+                        '40-49' => 1,
+                        '50-59' => 2,
+                        '60-69' => 1,
+                        '80-89' => 1,
+                    ],
+                    'female' => [
+                        '30-39' => 2,
+                        '50-59' => 6,
+                        '60-69' => 5,
+                        '70-79' => 2,
+                    ],
+                ],
+                'gender' => [
+                    'male' => 5,
+                    'female' => 15,
+                ],
+            ]
+        ];
+        return $this->json($data);
+    }
 
     private function serialize(array $data): JsonResponse
     {
